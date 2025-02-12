0bb0c2faa2c93691e0be78a14a82a0a8f3608c44
baconkak1 <natwarit.chu@student.mahidol.edu>
2025-02-12 08:43:32 +0000

    .

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int main(int argc , char **argv) {
             x=dequeue_struct(&q);
             printf("dequeing %d\n",x);
             }
-            else printf("Empty queue");
+            else printf("Empty queue\n");
             }
         else {
        enqueue_struct(&q, atoi(argv[i]));
